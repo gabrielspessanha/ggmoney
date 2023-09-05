97c9f2dd38a4f8e4f06f98e6178433b7f322b5d3
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-09-05 09:25:15 -0300

    Adicionando a tabela de transações

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Summary } from "../Summary"
+import { TransactionsTable } from "../TransactionsTable"
 import { Container } from "./styles"
 
 
@@ -8,6 +9,7 @@ export function Dashboard(){
     return(
         <Container>
             <Summary />
+            <TransactionsTable />
         </Container>
     )
 }
